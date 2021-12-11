5aebdbe8a764b2c5b5f1fa9bb1bb351e1ca5d69c
George <gogopro27@abv.bg>
2021-12-11 16:58:05 +0200

    LinearSearch impl.

---
+++ Linear_Search/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Linear_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rollsArr = { 1, 2, 3, 4, 5, 10, 22, 27 };
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            bool searchResult = false;
+
+            for (int i = 0; i < rollsArr.Length; i++)
+            {
+                if (rollsArr[i] == inputNumber)
+                {
+                    Console.WriteLine($"The number is found, {inputNumber} is in the array.");
+                    searchResult = true;
+                }           
+            }
+
+            if (searchResult == false)
+            {
+                Console.WriteLine($"The {inputNumber} was not in the array ");
+            }
+        }
+    }
+}
